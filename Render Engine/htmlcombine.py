a8d0797b749643f27a933e497e09c266cc6e01f9
YagiGo <jeremywu1995@gmail.com>
2018-02-26 12:55:38 +0900

    "Use libmproxy API now"

--- Render Engine/htmlcombine.py
+++ Render Engine/htmlcombine.py
@@ -3,6 +3,7 @@
 #  Get URL for css
 import re
 import sys, urlparse, os
+import urllib
 re_css_url = re.compile('(url\(.*?\))') #  get css url
 
 #  colored logging, at present, stderr is used, may shift to log file system
@@ -51,6 +52,37 @@ def absurl(index, relpath=None, normpath=None):
             return normpath(os.path.join(os.path.dirname(index), relpath))
         else:
             return index
+#  get web Content
+webpage2html_cache = {}
+def get(index, relpath=None, verbose=True, usecache=True, verify=True, ignore_error=False)
+    """
+
+    :param index:
+    :param relpath: relative path
+    :param verbose: use log system?
+    :param usecache: use cache?
+    :param verify: verify cache?
+    :param ignore_error:
+    :return:
+    """
+    global webpage2html_cache
+    if index.startwith('http') or (relpath and relpath.startwith('http')):
+        fullpath = absurl(index, relpath)
+        if not fullpath:
+            if verbose: log('[WARN] invalid path, %s %s' %(index, relpath), 'yellow')
+            return '', None
+        # urllib2 only accepts valid url, the following code is taken from urllib
+        # http://svn.python.org/view/python/trunk/Lib/urllib.py?r1=71780&r2=71779&pathrev=71780
+        #  按照标准， URL 只允许一部分 ASCII 字符（数字字母和部分符号），其他的字符（如汉字）是不符合 URL 标准的。
+        #  所以 URL 中使用其他字符就需要进行 URL 编码
+        fullpath = urllib.quote(fullpath, safe="%/:=&?~#+!$,;'@()*[]")
+        if usecache:
+            if fullpath in webpage2html_cache:
+                if verbose: log('[CACHE-HIT] -%s' %fullpath)
+                return webpage2html_cache[fullpath], None
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)'
+        }
 
 
 
