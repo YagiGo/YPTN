c15068a4e98329834b4542d8e8a800421bd31cd6
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-03-01 11:11:53 +0900

    "Remove unused files"

--- Render Engine/htmlcombine.py
+++ Render Engine/htmlcombine.py
@@ -357,7 +357,7 @@ def mergeHTML(url, output):
         page_cache[url] = rs
     with open(output, 'wb') as f:
         f.write(rs)
-    print page_cache
+    #  print page_cache
 
 
 
@@ -367,7 +367,7 @@ def mergeHTML(url, output):
 
 
 if __name__ == '__main__':
-    test_url1 = "http://www.softlab.cs.tsukuba.ac.jp/members.html"
+    test_url1 = "https://realpython.com/blog/python/introduction-to-mongodb-and-python/"
 
     test_url2 = "https://www.taobao.com/"
     test_url3 = "https://developer.mozilla.org/zh-CN/docs/Web/API/GlobalEventHandlers/onerror"
@@ -378,6 +378,6 @@ if __name__ == '__main__':
     end_time = time.time()
     print ("time cost: %s"%(str(end_time - start_time)))
 #  TODO 使用数据库（推荐MongoDB）缓存转换好的HTML文件，按照LRU算法对缓存文件进行更新，在用户访问某网站时直接调用缓存
-#  TODO 但是对很多实时性要求高的网站不能使用这个方法
+#  TODO 但是对很多实时性要求高的网站不能使用这个方法(SNS)
 #  TODO 对这类网站加上标签，然后直接访问，不经过HTML转换
 #  TODO 加标签的方法，emmmm 机器学习走一波
