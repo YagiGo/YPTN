daeda4acb5f49d2b31c51f1b0ccb970b70813c04
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-03-08 18:06:18 +0900

    "Emerging nodejs with Server push function on"

--- Render Engine/nodeserver.js
+++ Render Engine/nodeserver.js
@@ -33,3 +33,30 @@ emitter.on('someEvent', function(arg1, arg2) {
 });
 emitter.emit('someEvent', 'arg1', 'arg2');
 */
+var EventEmitter = require('events').EventEmitter;
+var eventEmitter = new EventEmitter();
+// listener 1
+var listener1 = function listener() {
+    console.log("listener 1 now running");
+};
+
+var listener2 = function listener() {
+    console.log("listener 2 now running");
+};
+
+eventEmitter.addListener('connection', listener1);
+eventEmitter.on('connection', listener2);
+
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log(eventListeners + " Listeners Running at Present");
+
+eventEmitter.emit('connection');
+
+eventEmitter.removeListener('connection', listener1);
+console.log('Listener1 quited');
+
+eventEmitter.emit('connection');
+eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log(eventListeners + " Listeners now running");
+
+console.log("Finished");
