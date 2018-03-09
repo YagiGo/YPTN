647ca1ac83fa924e4912eef157edaa6b3162b3ab
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-03-09 13:27:18 +0900

    "Emerging nodejs with Server push function on"

--- Render Engine/nodejscheatsheet.js
+++ Render Engine/nodejscheatsheet.js
@@ -207,6 +207,29 @@ console.log('Content: ' + buf18.toString());
 //buf.length 长度
 
 
+/*
+ */
+var fs = require('fs');
+var data = '';
+
+//创建可读流
+var readerStream = fs.createReadStream('input.txt');
+//设置编码为UTF8
+readerStream.setEncoding('UTF8');
+//处理流事件 --->data, end, and error
+readerStream.on('data', function(chunk) {
+    data += chunk;
+});
+
+readerStream.on('end', function() {
+    console.log(data);
+});
+
+readerStream.on('error', function(err) {
+    console.log(err.stack);
+});
+console.log("Finished");
+
 
 
 
