7203ff7a42077a11e14ca23c14a26a4d2aa83ead
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-03-09 12:02:24 +0900

    "Emerging nodejs with Server push function on"

--- Render Engine/nodejscheatsheet.js
+++ Render Engine/nodejscheatsheet.js
@@ -64,3 +64,155 @@ const buf = Buffer.from('test', 'ascii');
 console.log(buf.toString('hex'));
 console.log(buf.toString('base64'));
 
+//Buffer.alloc(size[, fill[, encoding]])： 返回一个指定大小的 Buffer 实例，如果没有设置 fill，则默认填满 0
+//长度10，填满0的Buffer
+const buf1 = Buffer.alloc(10);
+//长度10，填满0x1的Buffer
+const buf2 = Buffer.alloc(10, 1);
+//Buffer.allocUnsafe(size)： 返回一个指定大小的 Buffer 实例，但是它不会被初始化，所以它可能包含敏感的数据
+// 长度10， 未初始化的Buffer
+const buf3 = Buffer.allocUnsafe(10);
+
+// 创建一个包含 [0x1, 0x2, 0x3] 的 Buffer。
+const buf4 = Buffer.from([1, 2, 3]);
+
+// 创建一个包含 UTF-8 字节 [0x74, 0xc3, 0xa9, 0x73, 0x74] 的 Buffer。
+const buf5 = Buffer.from('tést');
+
+// 创建一个包含 Latin-1 字节 [0x74, 0xe9, 0x73, 0x74] 的 Buffer。
+const buf6 = Buffer.from('tést', 'latin1');
+
+
+
+//写入缓冲区和从缓冲区读取数据
+//写入缓冲区
+/*
+buf.write(string[, offset[, length]][, encoding])
+string - 写入缓冲区的字符串。
+
+offset - 缓冲区开始写入的索引值，默认为 0 。
+
+length - 写入的字节数，默认为 buffer.length
+
+encoding - 使用的编码。默认为 'utf8' 。
+
+返回实际写入的大小。如果 buffer 空间不足， 则只会写入部分字符串。
+
+ */
+const buf7 = Buffer.alloc(256);
+len = buf7.write('www.example.com');
+console.log(len + ' Bytes was written');
+
+//从缓冲区读取数据
+/*
+buf.toString([encoding[, start[, end]]])
+encoding - 使用的编码。默认为 'utf8' 。
+
+start - 指定开始读取的索引位置，默认为 0。
+
+end - 结束位置，默认为缓冲区的末尾。
+ */
+const buf8 = Buffer.alloc(26);
+for (var i = 0; i < 26; i++) {
+    buf8[i] = i + 97;
+}
+console.log(buf8.toString('ascii'));
+console.log(buf8.toString('ascii', 0, 5));
+console.log(buf8.toString('ascii', 0, 5));
+console.log(buf8.toString('utf8', 0, 5));
+
+// Buffer to JSON
+/*
+buf.toJSON()
+ */
+const buf9 = Buffer.from([0x1,0x2,0x3,0x4,0x5]);
+const json = JSON.stringify(buf9);
+// 输出: {"type":"Buffer","data":[1,2,3,4,5]}
+console.log(json);
+// =>箭头函数， 可以直接对参数进行调整
+/*
+(参数1, 参数2, …, 参数N) => {函数声明}
+(参数1, 参数2, …, 参数N) => 表达式（单一）
+//相当于：(参数1, 参数2, …, 参数N) =>{ return表达式}
+
+// 当只有一个参数时，圆括号是可选的：
+(单一参数) => {函数声明}
+单一参数 => {函数声明}
+
+// 没有参数的函数应该写成一对圆括号。
+() => {函数声明}
+ */
+
+//缓冲区合并
+/*
+Buffer.concat(list[, totalength)
+list - 用于合并的 Buffer 对象数组列表。
+
+totalLength - 指定合并后Buffer对象的总长度。
+
+ */
+var buf10 = Buffer.from(('123'));
+var buf11 = Buffer.from(('456'));
+var buf12 = Buffer.concat([buf10, buf11]);
+//Output: 123456
+console.log("buf12: " + buf12.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
